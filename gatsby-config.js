91b4c611c716e1a80dd4f514aeecde2321866461
Stephan Loh <sloh4137@gmail.com>
2020-05-29 16:38:37 -0400

    Configure gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,74 @@
+const urljoin = require("url-join");
+const config = require("./site-config");
+require("dotenv").config() // for later use with environment variables
+
 module.exports = {
+  pathPrefix: config.pathPrefix === "" ? "/" : config.pathPrefix,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    siteUrl: urljoin(config.pathPrefix, config.siteUrl),
+    title: config.siteTitle,
+    description: config.siteDescriptionShort,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-material-ui",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: "images",
+        path: "${__dirname}/src/images",
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "static",
+        path: `${__dirname}/static/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "events",
+        path: `${__dirname}/content/events/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "bios",
+        path: `${__dirname}/content/bios/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Poppins`,
+            variants: [`400`, `700`]
+          },
+        ],
       },
     },
+    "gatsby-plugin-netlify-cms",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleShort,
+        start_url: config.pathPrefix,
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: config.siteLogo, // This path is relative to the root of the site.
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    "gatsby-plugin-offline",
   ],
 }
--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ const config = {
     siteTitle: "UMCP TASE", // Site title.
     siteTitleShort: "UMCP TASE", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
     siteTitleAlt: "UMCP Taiwanese American Student Association", // Alternative site title for SEO.
-    siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+    siteLogo: "src/logos/logo-black.png", // Logo used for SEO and manifest.
     siteUrl: "umcptase.netlify.com", // Domain of your website without pathPrefix.
     pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
     fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
