3f073fe41acfb478ea111415ce2ee87091a95599
Stephan Loh <sloh4137@gmail.com>
2020-06-06 00:39:58 -0400

    Add contact us form

---
+++ src/components/ContactUs/Form.tsx
@@ -0,0 +1,147 @@
+import React, { useState } from "react";
+import { Button, CircularProgress, Theme, TextField, createStyles, withStyles, WithStyles } from "@material-ui/core";
+// Components
+
+const styles = (theme: Theme) => createStyles({
+    root: {
+        padding: theme.spacing(1),
+    },
+    submit: {
+        marginLeft: "auto",
+        marginRight: "auto",
+    },
+    backdrop: {
+        zIndex: theme.zIndex.drawer + 1,
+        color: '#fff',
+    },
+    success: {
+        color: theme.palette.primary.dark,
+        textAlign: "center",
+    },
+    failure: {
+        color: "#f44336",
+        textAlign: "center",
+    }
+});
+
+type State = {
+    name: string,
+    email: string,
+    subject: string,
+    body: string,
+}
+
+type SubmitDataType = Record<string, string>
+
+const encode = (data: SubmitDataType) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
+
+type Props = WithStyles<typeof styles>;
+
+function Form(props: Props) {
+    const { classes } = props;
+    const [state, setState] = useState<State>(
+        {
+            name: "",
+            email: "",
+            subject: "",
+            body: "",
+        }
+    )
+
+    const [submitContent, setSubmitContent] = useState(
+        <Button
+            className={classes.submit}
+            type="submit"
+            color="primary"
+            fullWidth
+        ><b>Submit</b></Button>
+    );
+
+    const handleChange = (event: React.FormEvent) => {
+        const target = event.target as HTMLInputElement;
+        setState({ ...state, [target.name]: target.value })
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        setSubmitContent(<CircularProgress color="primary" />);
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...state })
+        })
+            .then(() => {
+                setSubmitContent(<h3 className={classes.success}>Success! Thank you for your message!</h3>)
+            })
+            .catch(error => {
+                setSubmitContent(<h3 className={classes.failure}>Failed to send message! {error}</h3>)
+            });
+
+        event.preventDefault();
+    };
+
+    return (
+        <div className={classes.root}>
+            <form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <TextField
+                    required
+                    id="form-text-name"
+                    label="Name"
+                    name="name"
+                    variant="outlined"
+                    color="secondary"
+                    margin="dense"
+                    fullWidth
+                    value={state.name}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="form-text-email"
+                    label="Email"
+                    name="email"
+                    variant="outlined"
+                    color="secondary"
+                    margin="dense"
+                    fullWidth
+                    value={state.email}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="form-text-subject"
+                    label="Subject"
+                    name="subject"
+                    variant="outlined"
+                    color="secondary"
+                    margin="normal"
+                    fullWidth
+                    value={state.subject}
+                    onChange={handleChange}
+                />
+                <TextField
+                    required
+                    id="form-text-body"
+                    label="Body"
+                    name="body"
+                    variant="outlined"
+                    color="secondary"
+                    margin="normal"
+                    fullWidth
+                    multiline
+                    rows={6}
+                    value={state.body}
+                    onChange={handleChange}
+                />
+                {submitContent}
+            </form>
+        </div>
+
+    );
+}
+
+
+export default withStyles(styles)(Form)
--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,15 +1,35 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, PageProps } from "gatsby"
+import { Theme, createStyles, withStyles, WithStyles} from "@material-ui/core"
 
+// Components
 import SEO from "components/seo"
+import ContactUsForm from "components/ContactUs/Form"
 
-const ContactUsPage = () => (
-  <>
-    <SEO title="Contact Us" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </>
-)
+const styles = (theme: Theme) => createStyles({
+  root: {
+      top: 'auto',
+      bottom: 0,
+      padding: theme.spacing(1),
+  },
+  grow: {
+      flexGrow: 1,
+  },
+  copyright: {
+      marginLeft: theme.spacing(3),
+      color: theme.palette.primary.main,
+  },
+});
+
+type Props = PageProps & WithStyles<typeof styles>;
+
+function ContactUsPage(__: Props) {
+  return (
+    <>
+      <SEO title="Contact Us" />
+      <ContactUsForm />
+    </>
+  );
+}
 
 export default ContactUsPage
