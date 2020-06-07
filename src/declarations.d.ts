9c33d8ac1e979398a07dc733db2080ff0c1e45bd
Stephan Loh <sloh4137@gmail.com>
2020-06-06 23:22:37 -0400

    Add own declaration of parallax layer since it didn't accept classname before

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,10 +1,43 @@
 // This file is used to hold ambient type declarations, as well as type shims
 // for npm module without type declarations, and assets files.
 
+import { number } from "prop-types";
+
 // For example, to shim modules without declarations, use:
-declare module "gatsby-plugin-mailchimp" 
+declare module "gatsby-plugin-mailchimp"
 {
-    export default function addToMailchimp(email: string, fields?: any, endpointOverride?: any): {result: string, msg: string}
+    export default function addToMailchimp(email: string, fields?: any, endpointOverride?: any): { result: string, msg: string }
+}
+
+declare module "react-spring/renderprops-addons"
+{
+    interface ParallaxProps {
+        pages: number
+
+        config?: SpringConfig | ((key: string) => SpringConfig)
+
+        scrolling?: boolean
+
+        horizontal?: boolean
+
+        ref?: Ref<Parallax>
+    }
+
+    export class Parallax extends PureComponent<ParallaxProps> {
+        scrollTo: (offset: number) => void
+    }
+
+    interface ParallaxLayerProps {
+        factor?: number
+
+        offset?: number
+
+        speed?: number
+
+        className?: string,
+    }
+
+    export class ParallaxLayer extends PureComponent<ParallaxLayerProps> { }
 }
 
 // And to shim assets, use (one file extension per `declare`):
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,37 @@ import React from "react"
 import { PageProps } from "gatsby"
 import { Container, Theme, createStyles, withStyles, WithStyles } from "@material-ui/core"
 
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
+
 // Components
 import SEO from "components/seo"
 import EventsGrid from "components/Events/EventsGrid"
 import Welcome from "components/General/Welcome"
 
 const styles = (theme: Theme) => createStyles({
-  // Add styles here
+  firstBackground: {
+    backgroundColor: theme.palette.primary.main,
+  },
 });
 
 type Props = PageProps & WithStyles<typeof styles>;
 
-const IndexPage = (__: PageProps) => (
-  <Container maxWidth="xl">
-    <SEO title="Home" />
-    <Welcome />
-    <EventsGrid showDescription={true} />
-  </Container>
-)
+function IndexPage(props: Props) {
+  const { classes } = props;
+  return (
+    <>
+      <Parallax pages={3}>
+        <ParallaxLayer offset={1} speed={1} className={classes.firstBackground} />
+        <ParallaxLayer offset={2} speed={1} className={classes.firstBackground} />
+      </Parallax>
+      <Container maxWidth="xl">
+        <SEO title="Home" />
+        <Welcome />
+        <EventsGrid showDescription={true} />
+      </Container>
+    </>
+
+  );
+}
 
 export default withStyles(styles)(IndexPage)
