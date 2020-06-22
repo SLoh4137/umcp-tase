7e1d5a5115a99008a7251e59690f12fe9ea256bc
Stephan Loh <sloh4137@gmail.com>
2020-06-22 13:28:00 -0400

    Remove commented out parallax code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,17 +9,13 @@ import {
     WithStyles,
 } from "@material-ui/core"
 
-// https://www.react-spring.io/docs/props/parallax
-// import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons"
-
 // Components
 import SEO from "components/seo"
 import PastEventsGrid from "components/Events/PastEventsGrid"
 import Welcome from "components/General/Welcome"
 import PageContent from "components/Layout/PageContent"
 import ParallaxBackground from "components/General/ParallaxBackground"
-import Newsletter from "components/Mailchimp/Newsletter"
-import { Parallax } from "react-spring/renderprops-addons"
+// import Newsletter from "components/Mailchimp/Newsletter"
 
 const styles = (theme: Theme) =>
     createStyles({
@@ -74,33 +70,6 @@ function IndexPage(props: Props) {
             </PageContent>
         </>
     )
-
-    /* If you ever want to explore Parallax using react-spring, the code I started writing is below.
-     * I didn't like how it created a separate scroll container which only happens because we have the
-     * layout wrapping the entire page */
-
-    // return (
-    //     <>
-    //         <SEO title="Home" />
-    //         <Parallax pages={2}>
-    //             <ParallaxLayer offset={0} speed={1}>
-    //                 <Container maxWidth="xl">
-    //                     <Welcome />
-    //                 </Container>
-    //             </ParallaxLayer>
-
-    //             <ParallaxLayer
-    //                 offset={0.5}
-    //                 speed={1}
-    //                 className={classes.firstBackground}
-    //             />
-
-    //             <ParallaxLayer offset={0.5} speed={1}>
-    //                 <PastEventsGrid showDescription={true} />
-    //             </ParallaxLayer>
-    //         </Parallax>
-    //     </>
-    // )
 }
 
 export const query = graphql`
