52e22259d764846a9c4f86bfbb298003d252ef10
Stephan Loh <sloh4137@gmail.com>
2020-06-25 16:46:51 -0400

    Revert back to Roboto font

--- src/theme.tsx
+++ src/theme.tsx
@@ -25,7 +25,7 @@ const theme = createMuiTheme({
         }
     },
     typography: {
-        fontFamily: ["Poppins", "sans-serif"].join(","),
+        //fontFamily: ["Poppins", "sans-serif"].join(","),
     },
 })
 
