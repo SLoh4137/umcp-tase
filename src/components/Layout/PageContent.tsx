14c8814c8e7a756c1a79211a3c0302783c0da187
Stephan Loh <sloh4137@gmail.com>
2020-06-26 19:27:02 -0400

    Change page content s so small screens look better

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -1,10 +1,5 @@
 import React from "react"
-import {
-    Theme,
-    withStyles,
-    WithStyles,
-    createStyles,
-} from "@material-ui/core"
+import { Theme, withStyles, WithStyles, createStyles } from "@material-ui/core"
 import clsx from "clsx"
 
 const styles = (theme: Theme) =>
@@ -15,10 +10,12 @@ const styles = (theme: Theme) =>
             zIndex: 3,
         },
         mainRaised: {
-            margin: "-60px 30px 0px",
-            borderRadius: "6px",
-            boxShadow:
-                "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
+            [theme.breakpoints.up("sm")]: {
+                margin: "-60px 30px 0px",
+                borderRadius: "6px",
+                boxShadow:
+                    "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
+            },
         },
     })
 
@@ -29,9 +26,7 @@ type Props = WithStyles<typeof styles> & {
 function PageContent(props: Props) {
     const { classes, children } = props
     return (
-        <div className={clsx(classes.main, classes.mainRaised)}>
-            {children}
-        </div>
+        <div className={clsx(classes.main, classes.mainRaised)}>{children}</div>
     )
 }
 
