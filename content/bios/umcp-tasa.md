072fd603e8f85955dec332ec50942b2d7eeedcc0
Stephan Loh <sloh4137@gmail.com>
2020-07-03 21:58:19 -0400

    Change majors from string to a list

--- content/bios/umcp-tasa.md
+++ content/bios/umcp-tasa.md
@@ -3,7 +3,9 @@ name: UMCP TASA
 category: bio
 imgsrc: assets/jiufen-teahouse-night.jpg
 position: Co-President
-majors: Computer Science, Music
+majors: 
+  - Computer Science
+  - Music
 ---
 
 A wonderful position that can **test** many things
--- src/components/Bios/Bio.tsx
+++ src/components/Bios/Bio.tsx
@@ -40,22 +40,39 @@ function Bio(props: Props) {
     if (!node.frontmatter)
         throw new Error("Frontmatter does not exist for node")
 
-    const { name, position, majors } = node.frontmatter
-    const majorArray = majors ? majors.split(",") : []
+    const {
+        name = "Undefined",
+        position = "Undefined",
+        majors = [],
+    } = node.frontmatter
 
     return (
         <Card className={classes.root}>
             <div className={classes.content}>
                 <Img fluid={image.childImageSharp?.fluid} />
                 <CardContent>
-                    <Text variant="h5" color="textSecondary" className={classes.title}>{name}</Text>
-                    <Text variant="subtitle1" className={classes.position}>{position}</Text>
+                    <Text
+                        variant="h5"
+                        color="textSecondary"
+                        className={classes.title}
+                    >
+                        {name}
+                    </Text>
+                    <Text variant="subtitle1" className={classes.position}>
+                        {position}
+                    </Text>
                     <MarkdownContent content={node.html} />
                 </CardContent>
             </div>
             <CardActions className={classes.action}>
-                {majorArray.map((major) => (
-                    <Text variant="caption" className={classes.major} key={name + major}>{major}</Text>
+                {majors.map((major) => (
+                    <Text
+                        variant="caption"
+                        className={classes.major}
+                        key={name + major}
+                    >
+                        {major}
+                    </Text>
                 ))}
             </CardActions>
         </Card>
